9fcc1cee02f74a78a8e053c439ba5a3202e1cfa2
hsyoon <bagzein@gmail.com>
2023-07-08 16:32:02 +0900

    - assertion 추가하기 > 기본적으로 가정하고 있는 조건의 경우 Assertion을 사용하여 명시적으로 나타낸다.

--- src/main/java/me/whiteship/refactoring/_24_comments/_43_introduce_assertion/Customer.java
+++ src/main/java/me/whiteship/refactoring/_24_comments/_43_introduce_assertion/Customer.java
@@ -13,6 +13,10 @@ public class Customer {
     }
 
     public void setDiscountRate(Double discountRate) {
+        assert discountRate != null && discountRate > 0;    // but, assertion의 경우 JVM 옵션(-ea)을 끄면 통과하게 됨
+//        if (discountRate != null && discountRate > 0) {
+//            throw new IllegalArgumentException(discountRate + " can't be minus.");
+//        }
         this.discountRate = discountRate;
     }
 }
